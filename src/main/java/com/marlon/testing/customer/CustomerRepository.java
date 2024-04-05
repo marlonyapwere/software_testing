f254c3a49ad2ab31253647f46f1d9a0dcd0a08b6
marlone-nyapwere <marloneenyapwere@gmail.com>
2024-04-05 21:07:59 +0200

    JPA custom query

--- src/main/java/com/marlon/testing/customer/CustomerRepository.java
+++ src/main/java/com/marlon/testing/customer/CustomerRepository.java
@@ -1,8 +1,18 @@
 package com.marlon.testing.customer;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CustomerRepository extends CrudRepository<Customer, UUID> {
+
+    @Query(
+            value = "SELECT id, name, phone_number " +
+            "FROM Customer WHERE phone_number = :phone_number",
+            nativeQuery = true
+    )
+    Optional<Customer> selectCustomerByPhoneNumber(@Param("phone_number") String phone_number);
 }
